7f9f4ae9f92be6f9bd672cbe3cb28a673354d613
Chris Nasr <bast@maleexcel.com>
2020-06-17 11:38:54 -0400

    - Processed existing trigger files

--- scripts/tools/triggers.py
+++ scripts/tools/triggers.py
@@ -27,44 +27,7 @@ if __name__ == "__main__":
 
 	# Create an object to go through every existing trigger file
 	dTriggers = {
-		"INITIALS20200529.TXT": {"type": 'initials', "date": '2020-05-29 04:30:00'},
-		"REFILLS20200529.TXT": {"type": 'refills', "date": '2020-05-29 04:30:00'},
-		"INITIALS20200530.TXT": {"type": 'initials', "date": '2020-05-30 04:30:00'},
-		"REFILLS20200530.TXT": {"type": 'refills', "date": '2020-05-30 04:30:00'},
-		"INITIALS20200531.TXT": {"type": 'initials', "date": '2020-05-31 04:30:00'},
-		"REFILLS20200531.TXT": {"type": 'refills', "date": '2020-05-31 04:30:00'},
-		"INITIALS20200601.TXT": {"type": 'initials', "date": '2020-06-01 04:30:00'},
-		"REFILLS20200601.TXT": {"type": 'refills', "date": '2020-06-01 04:30:00'},
-		"INITIALS20200602.TXT": {"type": 'initials', "date": '2020-06-02 04:30:00'},
-		"REFILLS20200602.TXT": {"type": 'refills', "date": '2020-06-02 04:30:00'},
-		"INITIALS20200603.TXT": {"type": 'initials', "date": '2020-06-03 04:30:00'},
-		"REFILLS20200603.TXT": {"type": 'refills', "date": '2020-06-03 04:30:00'},
-		"INITIALS20200604.TXT": {"type": 'initials', "date": '2020-06-04 04:30:00'},
-		"REFILLS20200604.TXT": {"type": 'refills', "date": '2020-06-04 04:30:00'},
-		"INITIALS20200605.TXT": {"type": 'initials', "date": '2020-06-05 04:30:00'},
-		"REFILLS20200605.TXT": {"type": 'refills', "date": '2020-06-05 04:30:00'},
-		"TRIGGER20200606043000.TXT": {"type": None, "date": '2020-06-06 04:30:00'},
-		"TRIGGER20200606130000.TXT": {"type": None, "date": '2020-06-06 13:00:00'},
-		"TRIGGER20200607043000.TXT": {"type": None, "date": '2020-06-07 04:30:00'},
-		"TRIGGER20200607130000.TXT": {"type": None, "date": '2020-06-07 13:00:00'},
-		"TRIGGER20200608043000.TXT": {"type": None, "date": '2020-06-08 04:30:00'},
-		"TRIGGER20200608130000.TXT": {"type": None, "date": '2020-06-08 13:00:00'},
-		"TRIGGER20200609043000.TXT": {"type": None, "date": '2020-06-09 04:30:00'},
-		"TRIGGER20200609130000.TXT": {"type": None, "date": '2020-06-09 13:00:00'},
-		"TRIGGER20200610043000.TXT": {"type": None, "date": '2020-06-10 04:30:00'},
-		"TRIGGER20200610130000.TXT": {"type": None, "date": '2020-06-10 13:00:00'},
-		"TRIGGER20200611043000.TXT": {"type": None, "date": '2020-06-11 04:30:00'},
-		"TRIGGER20200611130000.TXT": {"type": None, "date": '2020-06-11 13:00:00'},
-		"TRIGGER20200612043000.TXT": {"type": None, "date": '2020-06-12 04:30:00'},
-		"TRIGGER20200612130000.TXT": {"type": None, "date": '2020-06-12 13:00:00'},
-		"TRIGGER20200613043000.TXT": {"type": None, "date": '2020-06-13 04:30:00'},
-		"TRIGGER20200613130000.TXT": {"type": None, "date": '2020-06-13 13:00:00'},
-		"TRIGGER20200614043000.TXT": {"type": None, "date": '2020-06-14 04:30:00'},
-		"TRIGGER20200614130000.TXT": {"type": None, "date": '2020-06-14 13:00:00'},
-		"TRIGGER20200615043000.TXT": {"type": None, "date": '2020-06-15 04:30:00'},
-		"TRIGGER20200615130000.TXT": {"type": None, "date": '2020-06-15 13:00:00'},
-		"TRIGGER20200616043000.TXT": {"type": None, "date": '2020-06-16 04:30:00'},
-		"TRIGGER20200616130000.TXT": {"type": None, "date": '2020-06-16 13:00:00'}
+		"TRIGGER20200617130000.TXT": {"type": None, "date": '2020-06-16 13:00:00'}
 	}
 
 	# Go through each file
@@ -72,6 +35,8 @@ if __name__ == "__main__":
 
 		# Open the file
 		with open('../Triggers/%s' % sFile) as oF:
+
+			print(sFile)
 
 			# Get a csv iterator
 			it = csv.reader(oF, delimiter=',', quotechar='"')
@@ -99,11 +64,15 @@ if __name__ == "__main__":
 					dRecord['type'] = lLine[0]
 					dRecord['customerId'] = lLine[11].lstrip('0')
 
+				print(dRecord)
+
 				# Create an instance of the record
 				try:
 					oTrigger = WdTrigger(dRecord);
 				except ValueError as e:
 					print(e)
+					print(dRecord)
+					sys.exit(1)
 
 				# Add the record to the DB
 				oTrigger.create(conflict='replace')
