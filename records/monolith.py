6be680d1f357511f02f7b34fe8881f99ad32d507
Chris Nasr <bast@maleexcel.com>
2020-08-12 16:19:17 -0400

    - Removed classes for tables that should no longer be accessible

--- records/monolith.py
+++ records/monolith.py
@@ -687,36 +687,6 @@ class KtOrder(Record_MySQL.Record):
 			Record_MySQL.ESelect.COLUMN
 		)
 
-# PharmacyFillError class
-class PharmacyFillError(Record_MySQL.Record):
-	"""PharmacyFillError
-
-	Represents an error while attempting to fill an order with a pharmacy
-	"""
-
-	_conf = None
-	"""Configuration"""
-
-	@classmethod
-	def config(cls):
-		"""Config
-
-		Returns the configuration data associated with the record type
-
-		Returns:
-			dict
-		"""
-
-		# If we haven loaded the config yet
-		if not cls._conf:
-			cls._conf = Record_MySQL.Record.generateConfig(
-				Tree.fromFile('definitions/monolith/pharmacy_fill_error.json'),
-				'mysql'
-			)
-
-		# Return the config
-		return cls._conf
-
 # ShippingInfo class
 class ShippingInfo(Record_MySQL.Record):
 	"""ShippingInfo
