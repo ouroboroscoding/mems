564146f94e3d14fa5fdeab9675043fc169b95988
Chris Nasr <bast@maleexcel.com>
2020-08-13 15:45:52 -0400

    - Fixed upgrade script to add triggers with no raw to manual adhoc rather than ignore them

--- upgrades/adhoc_from_trigger/__init__.py
+++ upgrades/adhoc_from_trigger/__init__.py
@@ -2,5 +2,5 @@
 from . import alter_adhoc, alter_fill_errors, copy_adhoc, create_adhoc_manual
 
 modules = [
-	alter_adhoc, copy_adhoc, alter_fill_errors, create_adhoc_manual
+	create_adhoc_manual, alter_adhoc, copy_adhoc, alter_fill_errors
 ]
