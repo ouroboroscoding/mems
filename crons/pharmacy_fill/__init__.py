640e5f85222a9636935ccc1dd00319f82cf1ca52
Chris Nasr <bast@maleexcel.com>
2020-09-11 09:15:53 -0400

    - Pharmacy Fill cron: fixed error when trying to make the CSVs for email reports. - Welldyne Not Opened report: fixed issue with SQL with never started.

--- crons/pharmacy_fill/__init__.py
+++ crons/pharmacy_fill/__init__.py
@@ -81,11 +81,11 @@ def _fill():
 					if dData['type'] == 'refill':
 
 						# If we don't have the pharmacy
-						if dData['pharmacy'] not in dReports:
-							dReports[dData['pharmacy']] = Generic.EmailFile(sEndTime)
+						if dData['pharmacy'] not in __mdReports:
+							__mdReports[dData['pharmacy']] = Generic.EmailFile()
 
 						# Add a line to the report
-						dReports[dData['pharmacy']].add(dData)
+						__mdReports[dData['pharmacy']].add(dData)
 
 		# Else, if it failed to process again
 		else:
@@ -160,7 +160,7 @@ def _fillErrors():
 
 							# If we don't have the pharmacy
 							if dData['pharmacy'] not in __mdReports:
-								__mdReports[dData['pharmacy']] = Generic.EmailFile(sEndTime)
+								__mdReports[dData['pharmacy']] = Generic.EmailFile()
 
 							# Add a line to the report
 							__mdReports[dData['pharmacy']].add(dData)
@@ -460,10 +460,12 @@ def run(period=None):
 		# If we're doing the early morning run
 		if period == 'morning':
 			sFileTime = '043000'
+			sEmailFile = '04:00:00'
 
 		# Else, if we're doing the mid day run
 		elif period == 'noon':
 			sFileTime = '130000'
+			sEmailFile = '12:30:00'
 
 		# Init the PharmacyFill module
 		PharmacyFill.initialise()
@@ -496,7 +498,7 @@ def run(period=None):
 		for sPharmacy,oEmail in __mdReports.items():
 
 			# Send the email
-			oEmail.send(sPharmacy, sEndTime)
+			oEmail.send(sPharmacy, sEmailFile)
 
 		# Regenerate the eligibility
 		WellDyne.eligibilityUpload(sFileTime)
