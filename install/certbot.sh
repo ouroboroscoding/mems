1b69c104ec2ef762c7fa0a4f54cd00d0a32f5046
Chris Nasr <bast@maleexcel.com>
2021-01-29 09:26:06 -0500

    - Install: added shell script for generating all SSL certificates

---
+++ install/certbot.sh
@@ -0,0 +1,19 @@
+apt-get -qq install software-properties-common
+add-apt-repository ppa:certbot/certbot
+apt-get -qq update
+apt-get -qq install python-certbot-nginx
+apt-get -qq install python-pip
+mkdir -p /root/venvs/certbot
+virtualenv -p /usr/bin/python3 /root/venvs/certbot
+
+# Custom cpanel dns authenticator
+/root/venvs/certbot/bin/pip install certbot-cpanel
+
+# MeMS and most UIs (meutils.com, *.meutils.com)
+/root/venvs/certbot/bin/certbot certonly --dns-route53 --dns-route53-propagation-seconds 30 -d meutils.com -d *.meutils.com --server https://acme-v02.api.letsencrypt.org/directory
+
+# Patient Portal (my.maleexel.com)
+/root/venvs/certbot/bin/certbot certonly --authenticator certbot-cpanel:dns-cpanel --certbot-cpanel:dns-cpanel-propagation-seconds 30 -d my.maleexcel.com --server https://acme-v02.api.letsencrypt.org/directory
+
+# Link Shortener (link.maleexcel.com)
+/root/venvs/certbot/bin/certbot certonly --authenticator certbot-cpanel:dns-cpanel --certbot-cpanel:dns-cpanel-propagation-seconds 30 -d link.maleexcel.com --server https://acme-v02.api.letsencrypt.org/directory
