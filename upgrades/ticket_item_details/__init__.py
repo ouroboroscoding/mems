0aabda4929b95530896ad1a97e13c963e33ac7d4
Chris Nasr <bast@maleexcel.com>
2021-06-26 11:15:01 -0400

    - CSR service: decided to leave direction as optional so we don't force people to update immediately.

--- upgrades/ticket_item_details/__init__.py
+++ upgrades/ticket_item_details/__init__.py
@@ -2,5 +2,5 @@
 from . import alter_table, create_tables, fill_tables, finalize_table, update_table
 
 modules = [
-	create_tables, fill_tables, alter_table, update_table, finalize_table
+	create_tables, fill_tables, alter_table, update_table
 ]
